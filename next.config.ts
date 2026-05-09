510a12a2f17cec5cb8e9e020644234c462af44db
Mahimna28 <mahimnamistry281005@gmail.com>
2026-05-09 16:07:46 +0530

    Force sequential build to fix memory crash

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
   /* config options here */
   reactCompiler: true,
 };
